c214e570f2312a2d91ca3e6c54e1ad3e1cf2481b
jose angel flores <joseangel.fflores@gmail.com>
2026-06-17 00:13:59 -0600

    initial react vite

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container mt-5">
+      <h1>Bienvenido al Home</h1>
+      <p>La navegación desde Login funciona correctamente.</p>
+    </div>
+  );
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (email === "admin" && password === "123456") {
+      navigate("/home");
+    } else {
+      alert("Credenciales incorrectas");
+    }
+  };
+
+  return (
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <div className="card shadow p-4" style={{ width: "400px" }}>
+        <h2 className="text-center mb-4">Iniciar Sesión</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Usuario</label>
+            <input
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Contraseña</label>
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button className="btn btn-primary w-100">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
